c276835f895c23331d38db6f4ccab168d7445c6b
Elmyrza Doolotbekov <elmyrza.do@gmail.com>
2023-03-30 15:33:51 +0600

    classwork1

---
+++ apps/cars/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+class Car(models.Model):
+    brand = models.CharField(
+        verbose_name= "Марка",
+        max_length= 200
+    )
+    model = models.CharField(
+        verbose_name= "Модель",
+        max_length= 200
+    )
+    year = models.PositiveSmallIntegerField(
+        verbose_name="Год выпуска"
+    )
+    color = models.CharField(
+        verbose_name= "Цвет",
+        max_length= 100
+    )
+    rudder_location = models.CharField(
+        verbose_name= "Расположение руля",
+        max_length= 100
+    )
+    engine_volume = models.PositiveSmallIntegerField(
+        verbose_name= "Обьем двигателя",
+        max_length= 200
+    )
+
+    def __str__(self):
+        return self.brand
+
+    class Meta:
+        verbose_name = "Машина"
+        verbose_name_plural = "Машины"
---
+++ apps/cars/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+
+from apps.cars.views import CarAPIView
+
+urlpatterns = [
+    path('cars/', CarAPIView.as_view(), name = 'api_cars')
+]
